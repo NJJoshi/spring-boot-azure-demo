6301f2054684f374074b0f1343195069e5d058ae
Nirav Joshi <joshi.nirav21@gmail.com>
2021-05-13 20:04:24 +0530

    first commit

---
+++ src/main/java/com/njtechie/springbootazuredemo/SpringBootAzureDemoApplication.java
@@ -0,0 +1,20 @@
+package com.njtechie.springbootazuredemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBootAzureDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAzureDemoApplication.class, args);
+    }
+
+    @GetMapping("/greet")
+    public String greet() {
+        return "Congratulation !! You have successfully deployed first spring boot app to Azure.";
+    }
+}
